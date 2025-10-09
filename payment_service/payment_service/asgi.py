0fe3470f6c5c4807132c56c57056564b9c4eb36a
steppacodes <steppacodes@gmail.com>
2025-10-09 07:53:14 +0100

    chore: services setup

---
+++ payment_service/payment_service/asgi.py
@@ -0,0 +1,8 @@
+
+import os
+from decouple import config
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'payment_service.settings.{config("SETTINGS")}')
+
+application = get_asgi_application()
